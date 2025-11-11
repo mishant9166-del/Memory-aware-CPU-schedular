ff741bc546ed818e55524827d8d0b56c6aebeb3d
Arryansh Messon <155538673+arryansh5@users.noreply.github.com>
2025-11-12 00:50:58 +0530

    Delete frontend directory

--- frontend/src/api/apiClient.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/api/apiClient.js
-import axios from "axios";
-
-const API_BASE = "http://127.0.0.1:8000";
-
-export const submitConfig = async (data) => {
-  return await axios.post(`${API_BASE}/config/submit`, data);
-};
